6ccc4181ea3d79c67b16a5da715cc2d2f5ef0d60
mahmutsel <mahmutsel29@gmail.com>
2020-10-15 11:54:34 +0100

    user authentication

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const profile = require('./routes/api/profiles');
+const posts = require('./routes/api/posts');
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+//Connect database
+
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
